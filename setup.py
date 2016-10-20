113671e591ca70050c75e266db3d44d65153899e
jan <janp@stanford.edu>
2016-10-19 17:00:00 -0700

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='masque',
-      version='0.1',
+      version='0.2',
       description='Lithography mask library',
       author='Jan Petykiewicz',
       author_email='anewusername@gmail.com',
