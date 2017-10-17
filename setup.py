7396e83f35816b1f50a620e98f8f1f57522c825f
Jan Petykiewicz <anewusername@gmail.com>
2017-10-17 12:56:06 -0700

    bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(name='masque',
-      version='0.2',
+      version='0.3',
       description='Lithography mask library',
       author='Jan Petykiewicz',
       author_email='anewusername@gmail.com',
