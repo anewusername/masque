bc5d4f62d8ba71ccba28362b8af94e1c6cbe3a3f
jan <jan@mpxd.net>
2017-11-04 12:18:58 -0700

    Test writing to gds in ellip_grating

--- examples/ellip_grating.py
+++ examples/ellip_grating.py
@@ -3,11 +3,12 @@
 import numpy
 
 import masque
+import masque.file.gdsii
 from masque import shapes
 
 
 def main():
-    pat = masque.Pattern()
+    pat = masque.Pattern(name='ellip_grating')
     for rmin in numpy.arange(10, 15, 0.5):
         pat.shapes.append(shapes.Arc(
             radii=(rmin, rmin),
@@ -15,7 +16,12 @@ def main():
             angles=(-numpy.pi/4, numpy.pi/4)
         ))
 
+    pat.scale_by(1000)
     pat.visualize()
+    pat2 = pat.copy()
+    pat2.name = 'grating2'
+
+    masque.file.gdsii.write_dose2dtype((pat, pat2), 'out.gds', 1e-9, 1e-3)
 
 
 if __name__ == '__main__':
