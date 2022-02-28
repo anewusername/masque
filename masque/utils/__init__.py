c8b4075fcc51cc69709016c375c9e5082d7c091f
jan <jan@orange.mpxd.net>
2022-02-27 21:26:51 -0800

    Avoid circular import

--- masque/utils/__init__.py
+++ masque/utils/__init__.py
@@ -10,6 +10,4 @@ from .bitwise import get_bit, set_bit
 from .vertices import remove_duplicate_vertices, remove_colinear_vertices
 from .transform import rotation_matrix_2d, normalize_mirror
 
-from . import pack2d
-
-
+#from . import pack2d
