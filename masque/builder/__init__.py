5f24ceb13f4945ba71eb2b1a7e580fa3d75e79e9
jan <jan@orange.mpxd.net>
2023-04-11 11:47:57 -0700

    add RenderPather

--- masque/builder/__init__.py
+++ masque/builder/__init__.py
@@ -1,5 +1,6 @@
 from .builder import Builder
 from .flatbuilder import FlatBuilder
 from .pather import Pather
+from .renderpather import RenderPather
 from .utils import ell
-from .tools import Tool
+from .tools import Tool, render_step_t
