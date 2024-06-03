31863c97996283436d4b97f6e55604fe4848f94e
Jan Petykiewicz <jan@mpxd.net>
2024-06-03 16:53:14 -0700

    reduce compression level to improve speed

--- masque/file/gdsii.py
+++ masque/file/gdsii.py
@@ -144,7 +144,7 @@ def writefile(
     with tmpfile(path) as base_stream:
         streams: tuple[Any, ...] = (base_stream,)
         if path.suffix == '.gz':
-            stream = cast(IO[bytes], gzip.GzipFile(filename='', mtime=0, fileobj=base_stream, mode='wb'))
+            stream = cast(IO[bytes], gzip.GzipFile(filename='', mtime=0, fileobj=base_stream, mode='wb', compresslevel=6))
             streams = (stream,) + streams
         else:
             stream = base_stream
